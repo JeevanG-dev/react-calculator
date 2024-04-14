35bbaaf43c3bfae95991724a69851f7df5511dd1
Jeevan Gaudel <jatingaudel01@gmail.com>
2024-04-14 19:23:40 +1000

    added few states and props to the buttoncontainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
 import Display from "./component/Display";
 import css from "./App.module.css";
 import ButtonContainer from "./component/ButtonContainer";
+import { useState } from "react";
 
 function App() {
+let [calVal,setCalVal] = useState("");
+
+let operator = ['%','*','/','+','-']
+
+function showValue (newBtn){
+if(newBtn === 'C'){
+
+  
+  setCalVal('');
+}
+
+else if (newBtn === '='){
+
+
+  const check = calVal[calVal.length -1]
+if(operator.includes(check)){
+  setCalVal(calVal.slice(0,-1))
+  
+}
+const reult = eval(calVal)
+setCalVal(reult)
+
+}
+
+
+
+else{
+  const newDisplayValue = calVal + newBtn;
+  setCalVal(newDisplayValue);
+}
+}
+
+  
+ 
+
   return (
     <div className={css.calculator}>
-      <Display></Display>
-      <ButtonContainer></ButtonContainer>
+      <Display displayValue={calVal}></Display>
+      <ButtonContainer onButtonClick={showValue}></ButtonContainer>
     </div>
   );
 }
--- src/component/ButtonContainer.jsx
+++ src/component/ButtonContainer.jsx
@@ -1,12 +1,32 @@
 import css from "./ButtonContainer.module.css";
 
-function ButtonContainer() {
-    const buttonNames =["C","1","2","+","3","4","-","5","6","*","7","8","/","=","9","0","."]
+function ButtonContainer({ onButtonClick }) {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
   return (
     <div className={css.buttonContainer}>
-
-        {buttonNames.map((buttonName => <button className={css.button}>{buttonName}</button>))}
-      
+      {buttonNames.map((buttonName) => (
+        <button className={css.button} onClick={()=> onButtonClick(buttonName)}>
+          {buttonName}
+        </button>
+      ))}
     </div>
   );
 }
